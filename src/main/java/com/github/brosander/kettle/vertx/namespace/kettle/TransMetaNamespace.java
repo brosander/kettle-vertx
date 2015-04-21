dd54c77299668f2a4eac34954316989e0f20e268
Bryan Rosander <brosander@pentaho.com>
2015-04-21 02:10:29 -0400

    adding trans meta namespace factory

--- src/main/java/com/github/brosander/kettle/vertx/namespace/kettle/TransMetaNamespace.java
+++ src/main/java/com/github/brosander/kettle/vertx/namespace/kettle/TransMetaNamespace.java
@@ -1,7 +1,9 @@
 package com.github.brosander.kettle.vertx.namespace.kettle;
 
+import com.github.brosander.kettle.vertx.namespace.Namespace;
 import com.github.brosander.kettle.vertx.namespace.factories.BeanMethodMapping;
 import com.github.brosander.kettle.vertx.namespace.factories.DelegatingNamespaceFactory;
+import com.github.brosander.kettle.vertx.namespace.factories.NamespaceFactory;
 import com.github.brosander.kettle.vertx.namespace.generic.BeanNamespace;
 import org.pentaho.di.trans.TransMeta;
 
@@ -15,5 +17,13 @@ public class TransMetaNamespace extends BeanNamespace {
     public TransMetaNamespace(String prefix, String name, Object object) {
         super(prefix, name, object, new DelegatingNamespaceFactory.Builder().addDelegate(STEP_METAS, new StepMetasNamespace.Factory()).build(),
                 new BeanMethodMapping.Builder(TransMeta.class).addSelfManagingProperty(STEP_METAS).addGetterMethod(STEP_METAS, GET_STEPS).build());
+    }
+
+    public static class Factory implements NamespaceFactory {
+
+        @Override
+        public Namespace create(String prefix, String name, Object object) {
+            return new TransMetaNamespace(prefix, name, object);
+        }
     }
 }
